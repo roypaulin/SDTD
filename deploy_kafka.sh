bd396bc57ec105cd2a5e4209b6785c4db078351d
marcioivan <marciofilho96@yahoo.com.br>
2020-01-28 17:21:04 +0100

    Fix on Kafka deploy and (almost finished) deploy for k8s

--- deploy_kafka.sh
+++ deploy_kafka.sh
@@ -17,23 +17,75 @@ BASE_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 KAFKA_DIR="${BASE_DIR}/kafka"
 
-# Load environment variables and compy templates
+if [ ! -d ${KAFKA_DIR}/utils ]; then
+  mkdir ${KAFKA_DIR}/utils
+fi
+
+# Installing terraform (if not already installed)
+echo -e "\n-> Checking if terraform is installed..."
+
+if [ -f ${KAFKA_DIR}/utils/terraform ]; then
+  TERRAFORM=${KAFKA_DIR}/utils/terraform
+elif [ -x "$(command -v terraform)" ]; then
+  TERRAFORM="$(command -v terraform)"
+else
+  echo -e "\nterraform not found. Installing terraform into folder kafka/utils..."
+
+  if [ "$(uname)" == "Darwin" ]; then # MacOS
+    TERRAFORM_FILE=terraform_0.12.12_darwin_amd64.zip
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then # Linux
+    TERRAFORM_FILE=terraform_0.12.12_linux_amd64.zip
+  else
+    echo -e "\nError: Unsupported OS. Please change to macOS or Linux.\n"
+    exit 1
+  fi
+
+  curl "https://releases.hashicorp.com/terraform/0.12.12/${TERRAFORM_FILE}}" -o "terraform.zip"
+
+  unzip terraform.zip -d ${KAFKA_DIR}/utils/
+
+  rm terraform.zip
+
+  chmod +x ${KAFKA_DIR}/utils/terraform
+
+  TERRAFORM=${KAFKA_DIR}/utils/terraform
+fi
+
+if [ -x "$(command -v ${TERRAFORM})" ]; then
+  echo -e "OK: terraform ready to be used!"
+else 
+  echo -e "\nProblem installing terraform.\n"
+  rm ${KAFKA_DIR}/utils/terraform >/dev/null 2>&1
+fi
+
+# Check if ansible is installed
+echo -e "\n-> Checking if ansible is installed..."
+
+if [ -x "$(command -v ansible-playbook)" ]; then
+  echo -e "OK: ansible ready to be used!"
+else 
+  echo -e "\nAnsible not installed. Please install the good version for your OS."
+  echo -e "Info: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html\n"
+fi
+
+# Load environment variables
 source ${BASE_DIR}/env/aws.env
+
+# Copy templates
 cp ${BASE_DIR}/env/twitter.env ${KAFKA_DIR}/ansible/roles/twitter.environment/templates/.env
-cp ${KAFKA_DIR}/twitter/twitter-publisher.py ${KAFKA_DIR}/ansible/roles/twitter.environment/templates/twitter-publisher.py
 
 # Check if asked to destroy
-if [ $1 == "destroy" ]; then
-  echo -e "\nDestroying kafka cluster..."
+if [ "$1" == "destroy" ]; then
+  echo -e "\n-> Destroying kafka cluster...\n"
   cd ${KAFKA_DIR}/terraform
-  terraform destroy -auto-approve
+  ${TERRAFORM} destroy -auto-approve
   if [ $? -eq 0 ]; then
   	rm ${KAFKA_DIR}/terraform/*.tfstate* > /dev/null 2>&1
     rm ${KAFKA_DIR}/terraform/.terraform > /dev/null 2>&1
     rm ${BASE_DIR}/SDTD-assembly-1.0.jar > /dev/null 2>&1
     rm ${BASE_DIR}/ssh_keys/kafka-key* > /dev/null 2>&1
     rm ${BASE_DIR}/ssh_config > /dev/null 2>&1
-  	echo -e "\n Kafka cluster removed!\n"
+  	echo -e "OK: Kafka cluster removed!\n"
   	exit 0
   else
     echo -e "\nProblem removing kafka cluster.\n"
@@ -42,34 +94,34 @@ if [ $1 == "destroy" ]; then
 fi
 
 # Create folder ssh_keys and delete previous keys (if they existed)
-if [ "$(ls -A ${BASE_DIR}/ssh_keys/kafka-key*)" ]; then
-  echo -e "\nRemoving old ssh-key..."
+if [ -f ${BASE_DIR}/ssh_keys/kafka-key ]; then
+  echo -e "\n-> Removing old ssh-key..."
   rm ${BASE_DIR}/ssh_keys/kafka-key*
 elif [ ! -d ${BASE_DIR}/ssh_keys ]; then
 	mkdir ${BASE_DIR}/ssh_keys
 fi
 
 # Create ssh keypair on folder ssh_keys
-echo -e "\nCreating new ssh key 'kafka-key' on folder ssh_keys\n"
+echo -e "\n-> Creating new ssh key 'kafka-key' on folder ssh_keys\n"
 ssh-keygen -t rsa -N "" -f ${BASE_DIR}/ssh_keys/kafka-key
 
-echo -e "\nCreating instances with Terraform..."
+echo -e "\n-> Creating instances with Terraform..."
 
 # Deploy instances with Terraform
 cd ${KAFKA_DIR}/terraform
 
-terraform init
-terraform plan
-terraform apply -auto-approve
+${TERRAFORM} init
+${TERRAFORM} plan
+${TERRAFORM} apply -auto-approve
 
 if [ ! $? -eq 0 ]; then
-	echo -e "\nTerraform apply failed. Check messages to fix problems."
+	echo -e "\nTerraform apply failed. Check messages to fix problems.\n"
 	exit 1
 fi
 
 sleep 20
 
-echo -e "\nInstalling Kafka using Ansible..."
+echo -e "\n-> Installing Kafka using Ansible...\n"
 
 # Install Kafka on instances
 cd ${KAFKA_DIR}/ansible
@@ -78,33 +130,28 @@ ansible-playbook all.yml
 
 if [ $? -eq 0 ]; then
 	mv ssh_config ${BASE_DIR}/
-  mv twitter.scala ${BASE_DIR}/
+  mv twitter-subscriber.scala ${BASE_DIR}/
 	echo -e "\nInstallion completed! You can access your machines with ssh -F ssh_config <host-name>"
 else
-	echo -e "\nInstallion failed. Check messages to fix problems."
+	echo -e "\nInstallion failed. Check messages to fix problems.\n"
+  exit 1
 fi
 
 cd ${BASE_DIR}
 
-echo -e "\nStarting kafka publisher..."
+echo -e "\n-> Starting kafka publisher..."
 
 # Start python script on control-center
-ssh -f -F ssh_config kafka-control-center-01 'nohup ./twitter-publisher.py &'
+ssh -f -F ssh_config kafka-control-center-01 'nohup python3 -u twitter-publisher.py > publisher.log &'
 
-echo -e "\nBuilding scala subscriber..."
+echo -e "\n-> Copying twitter-subscriber.scala to kubernetes/twitter/src/main/scala/ ..."
+
+if [ ! -d ${BASE_DIR}/kubernetes/twitter/src/main/scala ]; then
+  mkdir -p ${BASE_DIR}/kubernetes/twitter/src/main/scala >/dev/null 2>&1
+fi
 
 # Moving scala file created by ansible to good folder to build jar file
-cp twitter.scala ${KAFKA_DIR}/twitter/src/main/scala/
+mv  twitter-subscriber.scala ${BASE_DIR}/kubernetes/twitter/src/main/scala/
 
-cd ${KAFKA_DIR}/twitter
-
-# Build jar file
-sbt assembly
-
-if [ $? -eq 0 ]; then
-  echo -e "\nJar succesfully built!"
-  mv target/scala-2.11/SDTD-assembly-1.0.jar ${BASE_DIR}/
-  ./clean-scala.sh
-else
-  echo -e "\nProblem running sbt assembly.\n"
-fi
+echo -e "OK: file successfully copied to src folder from kubernetes!\n"
+exit 0
